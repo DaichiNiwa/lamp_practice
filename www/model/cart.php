5919df86b081026e5cb13c7d864b3aa31f00c160
DaichiNiwa <daichi.niwa.100@gmail.com>
2019-10-17 23:38:41 +0900

    #15 購入履歴がほぼ完成（新着順に並べ替えるのと、CSSを整えるのが必要

--- www/model/cart.php
+++ www/model/cart.php
@@ -131,17 +131,80 @@ function purchase_carts($db, $carts){
   if(validate_cart_purchase($carts) === false){
     return false;
   }
-  foreach($carts as $cart){
-    if(update_item_stock(
-        $db, 
-        $cart['item_id'], 
-        $cart['stock'] - $cart['amount']
-      ) === false){
-      set_error($cart['name'] . 'の購入に失敗しました。');
+  $db->beginTransaction();
+  try{
+    //購入履歴テーブルへのデータ保存
+    if(insert_histories($db, $carts[0]['user_id']) === false){
+      set_error('購入に失敗しました。');
     }
+    // 上の購入履歴のhistory_idを取得
+    $history_id = $db->lastInsertId();
+    foreach($carts as $cart){
+      // 購入詳細テーブルへのデータ保存と
+      // 在庫数を減らす
+      if(insert_purchased_carts($db, $history_id, $cart) === false ||
+        update_item_stock(
+          $db, 
+          $cart['item_id'], 
+          $cart['stock'] - $cart['amount']
+        ) === false){
+        set_error($cart['name'] . 'の購入に失敗しました。');
+      }
+    }
+    
+    // カート内の商品をすべて削除
+    if(delete_user_carts($db, $carts[0]['user_id']) === false){
+      set_error('購入に失敗しました。');
+    }
+    if(has_error() === true){
+      return false;
+    }
+    $db->commit();
+    return true;
+  }catch(PDOException $e){
+    $db->rollback();
+    return false;
   }
+}
+
+//購入履歴テーブルへのデータ保存
+function insert_histories($db, $user_id){
+  $sql = "
+    INSERT INTO
+      histories(
+        user_id
+      )
+    VALUES(:user_id)
+  ";
+  $params = array(':user_id' => $user_id);
   
-  delete_user_carts($db, $carts[0]['user_id']);
+  return execute_query($db, $sql, $params);
+}
+
+// 購入詳細テーブルへのデータ保存
+function insert_purchased_carts($db, $history_id, $cart){
+  $sql = "
+    INSERT INTO
+      purchased_carts(
+        history_id,
+        item_id,
+        amount,
+        purchased_price
+      )
+    VALUES(
+      :history_id, 
+      :item_id, 
+      :amount,
+      :purchased_price
+    )
+  ";
+  $params = array(
+    ':history_id' => $history_id,
+    ':item_id' => $cart['item_id'],
+    ':amount' => $cart['amount'],
+    ':purchased_price' => $cart['price']
+  );
+  return execute_query($db, $sql, $params);
 }
 
 function delete_user_carts($db, $user_id){
@@ -187,3 +250,27 @@ function validate_cart_purchase($carts){
   return true;
 }
 
+function get_histories($db, $user_id){
+  $sql = "
+  SELECT
+	  histories.history_id,
+    SUM(amount * purchased_price) as sum,
+    histories.created
+  FROM
+    histories
+  JOIN
+    purchased_carts
+  ON
+    histories.history_id = purchased_carts.history_id
+  WHERE
+    histories.user_id = :user_id
+  GROUP BY
+    histories.history_id;
+  ";
+
+  $params = array(
+    ':user_id' => $user_id
+  );
+
+  return fetch_all_query($db, $sql, $params);
+}
